bce2ed0ca326b5bf8bc094dede5337725d9486d5
Francis Conroy <francis.conroy@switchdin.com>
2024-06-02 21:16:24 +1000

    Add schedule config

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import tomllib
 from dataclasses import dataclass
+from typing import Tuple
 
 
 @dataclass
@@ -8,9 +9,20 @@ class AlbumsConfig:
     project_email: str
 
 
-def load_config(path) -> AlbumsConfig:
+@dataclass
+class ScheduleConfig:
+    time: str
+    timezone: str
+
+
+def load_config(path) -> Tuple[AlbumsConfig, ScheduleConfig]:
     with open(path, 'rb') as tomlfp:
         config_data = tomllib.load(tomlfp)
-        return AlbumsConfig(
-            project_name=config_data["1001_albums"].get("project_name", ""),
-            project_email=config_data["1001_albums"].get("project_email", ""))
+        return (
+            AlbumsConfig(
+                project_name=config_data["1001_albums"].get("project_name", ""),
+                project_email=config_data["1001_albums"].get("project_email",
+                                                             "")),
+            ScheduleConfig(time=config_data["schedule"].get("time", ""),
+                           timezone=config_data["schedule"].get("timezone",
+                                                                "")))
