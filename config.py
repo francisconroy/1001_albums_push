216f496084dca71ede95e237b802e4456a198925
Francis Conroy <francis.conroy@switchdin.com>
2024-06-07 13:23:38 +1000

    Remove email fields

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from typing import Tuple
 @dataclass
 class AlbumsConfig:
     project_name: str
-    project_email: str
 
 
 @dataclass
 class ScheduleConfig:
     time: str
     timezone: str
+
 
 @dataclass
 class NtfyConfig:
@@ -24,9 +24,8 @@ def load_config(path) -> Tuple[AlbumsConfig, ScheduleConfig, NtfyConfig]:
         config_data = tomllib.load(tomlfp)
         return (
             AlbumsConfig(
-                project_name=config_data["1001_albums"].get("project_name", ""),
-                project_email=config_data["1001_albums"].get("project_email",
-                                                             "")),
+                project_name=config_data["1001_albums"].get("project_name",
+                                                            "")),
             ScheduleConfig(time=config_data["schedule"].get("notify_time", ""),
                            timezone=config_data["schedule"].get("timezone",
                                                                 "")),
--- tests/test_config.py
+++ tests/test_config.py
@@ -12,7 +12,6 @@ class TestLoadConfig(unittest.TestCase):
 
         [1001_albums]
         project_name = "Test User"
-        project_email = "you@me.com"
         
         [schedule]
         timezone = "Australia/Sydney"
@@ -26,7 +25,6 @@ class TestLoadConfig(unittest.TestCase):
     def test_load_config(self):
         album_config, schedule_config, ntfy_config = load_config(self.temp_file.name)
         self.assertEqual("Test User", album_config.project_name)
-        self.assertEqual("you@me.com", album_config.project_email)
         self.assertEqual("Australia/Sydney", schedule_config.timezone)
         self.assertEqual("09:00", schedule_config.time)
         self.assertEqual("1001_albums_francis-conroy_effect_until_behavior_want", ntfy_config.topic)
