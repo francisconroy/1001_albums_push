9e0c166b4b665f7a8334633ab4bb7310c6949808
Francis Conroy <francis.conroy@switchdin.com>
2024-06-15 13:29:58 +1000

    Load the streaming service from the config file

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import enum
 import tomllib
 from dataclasses import dataclass
 from typing import Tuple
@@ -16,6 +17,7 @@ class StreamingServices(enum.StrEnum):
 @dataclass
 class AlbumsConfig:
     project_name: str
+    streaming_service: StreamingServices
 
 
 @dataclass
@@ -35,9 +37,12 @@ def load_config(path) -> Tuple[AlbumsConfig, ScheduleConfig, NtfyConfig]:
         return (
             AlbumsConfig(
                 project_name=config_data["1001_albums"].get("project_name",
-                                                            "")),
-            ScheduleConfig(time=config_data["schedule"].get("notify_time", ""),
-                           timezone=config_data["schedule"].get("timezone",
-                                                                "")),
+                                                            ""),
+                streaming_service=StreamingServices(
+                    config_data["1001_albums"].get("streaming_service",))),
+            ScheduleConfig(
+                time=config_data["schedule"].get("notify_time", ""),
+                timezone=config_data["schedule"].get("timezone",
+                                                     "")),
             NtfyConfig(topic=config_data["ntfy"].get("topic", ""))
         )
